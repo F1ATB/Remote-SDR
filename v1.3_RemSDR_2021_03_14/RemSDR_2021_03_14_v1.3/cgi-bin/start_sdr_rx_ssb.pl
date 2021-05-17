a1573844012346ab764858240d15f46ac0565978
F1ATB <andre.buhart@gmail.com>
2021-05-17 15:59:54 +0200

    Delete v1.3_RemSDR_2021_03_14 directory

--- v1.3_RemSDR_2021_03_14/RemSDR_2021_03_14_v1.3/cgi-bin/start_sdr_rx_ssb.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl 
-
-
-system "sudo python2  /home/sdr/Desktop/RemSDR/PY/remote_rx_ssb_v1.py";
-
-
-
--- v1.3_RemSDR_2021_03_14/RemSDR_2021_03_14_v1.3/cgi-bin/start_sdr_tx_ssb.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl 
-
-
-system "sudo python2  /home/sdr/Desktop/RemSDR/PY/remote_tx_ssb_v1.py";
-
-
-
